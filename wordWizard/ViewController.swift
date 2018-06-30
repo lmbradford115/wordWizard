43c1d8397c014a55677b277dea6189b068b90a39
lmbradford115 <lm.bradford115@gmail.com>
2018-06-30 14:57:25 -0500

    Initial Commit

---
+++ wordWizard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wordWizard
+//
+//  Created by Luke Bradford on 6/30/18.
+//  Copyright © 2018 Luke Bradford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
