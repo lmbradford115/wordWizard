01b3f847c67be9379679ba38d295f63f84fcfad5
lmbradford115 <lm.bradford115@gmail.com>
2018-12-07 18:30:03 -0600

    updated letter tile buttonss

--- wordWizard/ViewController.swift
+++ wordWizard/ViewController.swift
@@ -28,16 +28,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var button15: UIButton!
     @IBOutlet weak var button16: UIButton!
     
-    
+    var buttons: [UIButton] = [UIButton]();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // What is a nib?
+        
+        self.buttons = [self.button1, self.button2, self.button3, self.button4, self.button5, self.button6, self.button7, self.button8, self.button9, self.button10, self.button11, self.button12, self.button13, self.button14, self.button15, self.button16];
+        
+        for button in self.buttons {
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 45);
+            let finalTile = updateTile();
+            button.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateTile() -> (finalLetter: String, finalNewStr: String) {
+        var finalLetter = "";
+        var finalNewStr = "";
+        let tile = assignLetter();
+        let myPoints = Array(String(tile.points));
+        var newStr = "";
+        
+        //TEST
+        let subs = [
+            "0": "\u{2080}",
+            "1": "\u{2081}",
+            "2": "\u{2082}",
+            "3": "\u{2083}",
+            "4": "\u{2084}",
+            "5": "\u{2085}",
+            "6": "\u{2086}",
+            "7": "\u{2087}",
+            "8": "\u{2088}",
+            "9": "\u{2089}"];
+        for char in myPoints {
+            let key = String(char);
+            if subs.keys.contains(key){
+                newStr.append(Character(subs[key]!));
+            }
+        }
+        
+        finalLetter = tile.letter;
+        finalNewStr = newStr;
+        return (finalLetter, finalNewStr);
     }
     
     func assignLetter() -> (letter: String, points: Int)  {
@@ -135,80 +175,86 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func button1(_ sender: UIButton) {
-        let tile = assignLetter();
-        let myPoints = Array(String(tile.points));
-        var newStr = "";
-        
-        //TEST
-        let subs = [
-            "0": "\u{2080}",
-            "1": "\u{2081}",
-            "2": "\u{2082}",
-            "3": "\u{2083}",
-            "4": "\u{2084}",
-            "5": "\u{2085}",
-            "6": "\u{2086}",
-            "7": "\u{2087}",
-            "8": "\u{2088}",
-            "9": "\u{2089}"];
-        for char in myPoints {
-            let key = String(char);
-            if subs.keys.contains(key){
-                newStr.append(Character(subs[key]!));
-            }
-        }
-        button1.setTitle(tile.letter + newStr, for: .normal)
-        
-        
-        
+        let finalTile = updateTile();
+        button1.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
 
         //TEST
         //button1.setTitle(tile.letter + " " + String(tile.points), for: .normal)
     }
     
     @IBAction func button2(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button2.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button3(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button3.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button4(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button4.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button5(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button5.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button6(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button6.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button7(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button7.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button8(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button8.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button9(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button9.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button10(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button10.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button11(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button11.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button12(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button12.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button13(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button13.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button14(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button14.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button15(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button15.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     @IBAction func button16(_ sender: UIButton) {
+        let finalTile = updateTile();
+        button16.setTitle(finalTile.finalLetter + finalTile.finalNewStr, for: .normal);
     }
     
     
